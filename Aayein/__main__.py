ed5633a19a610f098128334fe20ac90607ac000e
Buddy <203337004+iambuddy01@users.noreply.github.com>
2026-03-14 17:05:10 +0530

    ᴅᴇᴠɴʙᴏᴛs

---
+++ Aayein/__main__.py
@@ -0,0 +1,10 @@
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+bot = Client(
+    "AccountManagerBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="Aayein.modules")
+)
